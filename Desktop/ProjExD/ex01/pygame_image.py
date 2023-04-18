c89381e854f1a98e8b9d954b446110134f7ba7bf
izumi <c0b22012ca@edu.teu.ac.jp>
2023-04-18 16:31:44 +0900

    演習第１問

--- Desktop/ProjExD/ex01/pygame_image.py
+++ Desktop/ProjExD/ex01/pygame_image.py
@@ -7,26 +7,30 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 =pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
-    kk_imgc=pg.transform.rotozoom(kk_img,0,1.0)
+    kk_imgc=pg.transform.rotozoom(kk_img,10,1.0)
     sur=[kk_img,kk_imgc]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        kk_imgc=pg.transform.rotozoom(kk_img,r,1.0)
+        
         tmr += 1
         if r<=10:
-            r+=1
+            r+=0.1
         elif r>10:
-            r-=1
-        x=(tmr%1600)-1
+            r-=0.1
+        x=(tmr%1600)
         screen.blit(bg_img,[-x,0])
-        screen.blit(bg_img,[1600-x,0])
+        screen.blit(bg_img2,[1600-x,0])
         
-        
-        screen.blit(kk_imgc,[300,200])
+        if tmr%20<=8:
+
+            screen.blit(sur[0],[300,200])
+        elif tmr%20>8:
+            screen.blit(sur[1],[300,200])
         pg.display.update()
         clock.tick(100)
 
