f7abdd4aad6732f8a5b1f0c4374a8bd95a70a11a
izumi <c0b22012ca@edu.teu.ac.jp>
2023-04-18 17:34:17 +0900

    演習第２問

--- Desktop/ProjExD/ex01/pygame_image.py
+++ Desktop/ProjExD/ex01/pygame_image.py
@@ -13,21 +13,17 @@ def main():
     kk_imgc=pg.transform.rotozoom(kk_img,10,1.0)
     sur=[kk_img,kk_imgc]
     tmr = 0
+    count=-1
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
         tmr += 1
-        if r<=10:
-            r+=0.1
-        elif r>10:
-            r-=0.1
-        x=(tmr%1600)
+        x=(tmr%3200)
         screen.blit(bg_img,[-x,0])
         screen.blit(bg_img2,[1600-x,0])
-        
+        screen.blit(bg_img,[3200-x,0])
         if tmr%20<=8:
-
             screen.blit(sur[0],[300,200])
         elif tmr%20>8:
             screen.blit(sur[1],[300,200])
